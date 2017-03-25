0988de028931ee0ef38a5f57d94a6392b7d8488d
Hari Bhrugubanda <hbhrugubanda@gmail.com>
2017-03-25 23:37:36 +1100

    hari - added async await and class wrapper for cassandra driver

--- db.js
+++ db.js
@@ -1,48 +1,57 @@
 const cassandra = require('cassandra-driver');
 const async = require('async');
 
-// Create keyspace query
-//  CREATE KEYSPACE IF NOT EXISTS demo WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1' }
-
 // Create users table
 const KEYSPACE = 'patchwork';
 const HOST = '127.0.0.1';
 
-const client = new cassandra.Client({contactPoints: [HOST], keyspace: KEYSPACE});
-// Use async series to run functions in serial (one after another)
-async.series([
-    // Insert Bob
-    function (callback) {
-        client.execute("INSERT INTO users (email, first_name, last_name) VALUES ('hari.b@example.com', 'Hari', 'B')", function (err, result) {
-            // Run next function in series
-            if (err) {
-              console.log(err);
-              return
-            }
-            console.log('inserted a row');
-            callback(err, null);
-        });
-    },
-    // Read users and print to console
-    function (callback) {
-        client.execute("SELECT first_name, last_name, email FROM users WHERE email='hari.b@example.com'", function (err, result) {
-            if (!err){
-                if ( result.rows.length > 0 ) {
-                    var user = result.rows[0];
-                    console.log(`name = ${user.first_name}, email = ${user.email}`);
-                } else {
-                    console.log("No results");
-                }
-            } else {
-              console.log(err);
-              return;
-            }
+module.exports = class dbClient {
+  constructor () {
+    this.client = new cassandra.Client({contactPoints: [HOST], keyspace: KEYSPACE});
+  }
 
-
-            // Run next function in series
-            callback(err, null);
-        });
+  async get (user) {
+    try {
+      let result = await client.execute(`"SELECT first_name, last_name, email FROM users WHERE email='${user.email}'`);
+      if ( result.rows.length > 0 ) {
+          var user_result = result.rows[0];
+          console.log(`name = ${user_result.first_name}, email = ${user_result.email}`);
+          return user_result
+      } else {
+          console.log("No results");
+          return;
+      }
+    } catch (err) {
+      console.log(err);
     }
+  }
+
+  async insert (user) {
+    try {
+      let result = await client.execute(`INSERT INTO users (email, first_name, last_name) VALUES ('${user.email}', '${user.first_name}', '${user.last_name}')`);
+      console.log('inserted a row');
+      return true;
+    } catch (err) {
+      // Run next function in series
+      console.log(err);
+      return false;
+    }
+  }
+
+  delete () {
+    console.log("delete not implemented.")
+  }
+
+  update () {
+    console.log("update not implemented.")
+  }
+}
+
+
+
+
+// Use async series to run functions in serial (one after another)
+// async.series([
     // Delete Bob
     // function (callback) {
     //     client.execute("DELETE FROM users WHERE lastname = 'Jones'", function (err, result) {
@@ -68,7 +77,7 @@ async.series([
     //         callback(err, null);
     //     });
     // }
-], function (err, results) {
-      // All finished, quit
-      process.exit();
-});
+// ], function (err, results) {
+//       // All finished, quit
+//       process.exit();
+// });
